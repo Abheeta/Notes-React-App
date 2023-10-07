0cff66623943912c2c0ee5d51c0f8abdf94d90b7
Abheeta <55648880+Abheeta@users.noreply.github.com>
2023-10-07 15:48:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,14 @@ app.use(cors());
 app.use("/api/notes", router);
 
 
+    mongoose.connect(process.env.MONGO_URI);
+
+
 
 
 app.listen(process.env.PORT || 8000, () => {
     console.log("server is running on port 8000");
-
-    mongoose.connect(process.env.MONGO_URI);
+    
 })
 
 
