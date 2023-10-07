b2f12f292935a87fb9f7aa561a2207bd4188a951
Abheeta <55648880+Abheeta@users.noreply.github.com>
2023-10-07 17:21:49 +0530

    Update server.js ppo

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 8000
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${encodeURIComponent(process.env.PASSWORD)}@cluster0.awo2daj.mongodb.net/?retryWrites=true&w=majority`);
+    const conn = await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.awo2daj.mongodb.net/?retryWrites=true&w=majority`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
